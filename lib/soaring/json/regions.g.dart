a5b6368a178928f7748c5eb7417f713ba75f9d4e
Eric Foertsch <efoertsch@fisincorporated.com>
2019-10-07 16:53:27 -0400

    Added auto generated regions code.

---
+++ lib/soaring/json/regions.g.dart
@@ -0,0 +1,103 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'regions.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Regions _$RegionsFromJson(Map<String, dynamic> json) {
+  return Regions(
+    initialRegion: json['initialRegion'] as String,
+    regions: (json['regions'] as List)
+        ?.map((e) =>
+            e == null ? null : Region.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    airspace: json['airspace'] == null
+        ? null
+        : Airspace.fromJson(json['airspace'] as Map<String, dynamic>),
+  );
+}
+
+Map<String, dynamic> _$RegionsToJson(Regions instance) => <String, dynamic>{
+      'initialRegion': instance.initialRegion,
+      'regions': instance.regions,
+      'airspace': instance.airspace,
+    };
+
+Region _$RegionFromJson(Map<String, dynamic> json) {
+  return Region(
+    dates: (json['dates'] as List)?.map((e) => e as String)?.toList(),
+    name: json['name'] as String,
+    printDates: (json['printDates'] as List)?.map((e) => e as String)?.toList(),
+    soundings: (json['soundings'] as List)
+        ?.map((e) =>
+            e == null ? null : Soundings.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+  );
+}
+
+Map<String, dynamic> _$RegionToJson(Region instance) => <String, dynamic>{
+      'dates': instance.dates,
+      'name': instance.name,
+      'printDates': instance.printDates,
+      'soundings': instance.soundings,
+    };
+
+Soundings _$SoundingsFromJson(Map<String, dynamic> json) {
+  return Soundings(
+    location: json['location'] as String,
+    longitude: json['longitude'] as String,
+    latitude: json['latitude'] as String,
+  );
+}
+
+Map<String, dynamic> _$SoundingsToJson(Soundings instance) => <String, dynamic>{
+      'location': instance.location,
+      'longitude': instance.longitude,
+      'latitude': instance.latitude,
+    };
+
+Airspace _$AirspaceFromJson(Map<String, dynamic> json) {
+  return Airspace(
+    baseUrl: json['baseUrl'] as String,
+    files: (json['files'] as List)?.map((e) => e as String)?.toList(),
+  );
+}
+
+Map<String, dynamic> _$AirspaceToJson(Airspace instance) => <String, dynamic>{
+      'baseUrl': instance.baseUrl,
+      'files': instance.files,
+    };
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+class _RaspClient implements RaspClient {
+  _RaspClient(this._dio) {
+    ArgumentError.checkNotNull(_dio, '_dio');
+  }
+
+  final Dio _dio;
+
+  final String baseUrl = 'https://soargbsc.com/rasp/';
+
+  @override
+  getRegions() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        'current.json',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = Regions.fromJson(_result.data);
+    return Future.value(value);
+  }
+}
