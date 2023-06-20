1623eccf576a95acb237a720f10c5e2682d01ae5
Eric Foertsch <ericfoertsch@gmail.com>
2023-06-20 19:52:13 -0400

    Corrected 'About' text. Fixed Setting Initial Forecast Hour to match to install default. Added progress spinner for GEOS image loader. Updated version and build

--- lib/soaring/satellite/geos/geos.dart
+++ lib/soaring/satellite/geos/geos.dart
@@ -73,11 +73,24 @@ class _GeosScreenState extends State<GeosScreen> {
         child: InteractiveViewer(
           boundaryMargin: const EdgeInsets.all(4.0),
           minScale: 1.0,
-          maxScale: 3.0,
+          maxScale: 4.0,
           child: Image.network(
             _displayCurrent ? GEOS_CURRENT : GEOS_GIF,
             fit: BoxFit.fitWidth,
-          ),
+            loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+              if (loadingProgress == null)
+                return child;
+              return Center(
+                child: CircularProgressIndicator(
+                  value: loadingProgress.expectedTotalBytes != null
+                      ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
+                      : null,
+                ),
+              );
+            },
+            errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+              return Text('Failed to load image');
+            },),
         ),
       ),
     );
